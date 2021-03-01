cff6624efca1925380dcc5ab7e7f27f29f2f0393
kszwarc <krzysztof@szwarc.net.pl>
2021-03-01 12:55:08 +0100

    Part2 added

---
+++ src/com/company/Part2.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Part2 {
+    /** To jest nasza metoda */
+    public static void main(String[] args) {
+        //// Zadanie 1
+        float floatNumber;
+        floatNumber = 0.1f;
+        double doubleNumber = floatNumber;
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        /// Zadanie 2
+        char var1 = 's';
+        char var2 = '\u0073';
+        System.out.println(var1);
+        System.out.println(var2);
+        /// Zadanie 3
+        long varLong = 5000000000L;
+        System.out.println("Zainicjowałem zmienną. Jej wartość to: " + varLong);
+        /// Zadanie 4
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("*******");
+        System.out.println("   *");
+        System.out.println("   *");
+        /// Zadanie 5
+        System.out.print("Ala\nma");
+        /// Zadanie 6
+        final boolean CONSTANT = true;
+        /// CONSTANT = false;
+        /// Zadanie 7
+        // float variable, variable; - NIE DA SIĘ
+        /// Zadanie 8
+        float variable, Variable;
+        /// Zadanie 9 - to jest komentarz
+    }
+}
