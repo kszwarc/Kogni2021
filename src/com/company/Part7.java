9f411ce122158d54b37727d5858c221d3fcba7ef
kszwarc <krzysztof@szwarc.net.pl>
2021-04-26 12:59:44 +0200

    Part 7 added

--- src/com/company/Part7.java
+++ src/com/company/Part7.java
@@ -1,7 +1,29 @@
 package com.company;
 
+import java.util.Random;
+
 public class Part7 {
     public static void main(String[] args) {
-
+        final int TAB_SIZE = 20;
+        double[] tab = new double[TAB_SIZE];
+        double sum = 0;
+        double min = 0;
+        double max = 0;
+        Random random = new Random();
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = random.nextDouble();
+            sum += tab[i];
+            if (i == 0) {
+                min = tab[i];
+                max = tab[i];
+            } else if (min > tab[i]) {
+                min = tab[i];
+            } else if (max < tab[i]) {
+                max = tab[i];
+            }
+        }
+        System.out.println("Średnia: " + sum / tab.length);
+        System.out.println("Minimalna: " + min);
+        System.out.println("Maksymalna: " + max);
     }
 }
